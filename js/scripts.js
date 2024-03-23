8b060e0eb32cc870721879648a2b23e82ac5c34c
jp117 <john.paradise117@gmail.com>
2024-03-23 19:09:35 -0400

    Added custom board size up to 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 let board = document.getElementById("board");
+let boardButton = document.getElementById("boardButton");
+let boardSize = document.getElementById("boardSize");
 
 function addBoardDiv(){
   const newDiv = document.createElement("div");
@@ -6,12 +8,12 @@ function addBoardDiv(){
   console.log(newDiv)
 }
 
-function initialBoard() {
-  for (let i = 0; i<16; i++) {
+function initialBoard(x) {
+  for (let i = 0; i<x; i++) {
     let boardRow = document.createElement("div");
     boardRow.className = "boardRow"
     board.appendChild(boardRow);
-    for (let x = 0; x<16; x++){
+    for (let y = 0; y<x; y++){
       let boardSquare = document.createElement("div");
       boardSquare.className = "boardSquare";
       boardRow.appendChild(boardSquare);
@@ -19,12 +21,23 @@ function initialBoard() {
   }
 }
 
-initialBoard();
+initialBoard(16);
+draw();
 
-const hoverBoard = document.getElementsByClassName("boardSquare");
-
-for (let hoverArray = 0; hoverArray < hoverBoard.length; hoverArray++){
-  hoverBoard[hoverArray].addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = 'black';
-  })
+boardButton.onclick = (event) => {
+  board.innerHTML = '';
+  if (boardSize.value >100){ boardSize.value = 100};
+  initialBoard(boardSize.value);
+  draw();
 }
+
+function draw() {
+  const hoverBoard = document.getElementsByClassName("boardSquare");
+  for (let hoverArray = 0; hoverArray < hoverBoard.length; hoverArray++){
+    hoverBoard[hoverArray].addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = 'black';
+    })
+  } 
+}
+
+
