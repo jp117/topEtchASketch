101e93a61ac33ff9368e729afd3f3996d5ab9cdd
jp117 <john.paradise117@gmail.com>
2024-03-23 18:28:07 -0400

    Added hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,23 @@ function addBoardDiv(){
 
 function initialBoard() {
   for (let i = 0; i<16; i++) {
-    console.log("i'm here");
     let boardRow = document.createElement("div");
     boardRow.className = "boardRow"
     board.appendChild(boardRow);
     for (let x = 0; x<16; x++){
       let boardSquare = document.createElement("div");
       boardSquare.className = "boardSquare";
-      console.log("i'm in boardSquare");
       boardRow.appendChild(boardSquare);
     }
   }
 }
 
 initialBoard();
+
+const hoverBoard = document.getElementsByClassName("boardSquare");
+
+for (let hoverArray = 0; hoverArray < hoverBoard.length; hoverArray++){
+  hoverBoard[hoverArray].addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = 'black';
+  })
+}
