2be6f309ac8550aa98e0fd3309549af4df979a46
jp117 <john.paradise117@gmail.com>
2024-03-23 18:05:44 -0400

    Set up initial board

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-console.log("if the background is there and you can see this, everything worked")
+let board = document.getElementById("board");
+
+function addBoardDiv(){
+  const newDiv = document.createElement("div");
+  newDiv.className = "boardSquare"
+  console.log(newDiv)
+}
+
+function initialBoard() {
+  for (let i = 0; i<16; i++) {
+    console.log("i'm here");
+    let boardRow = document.createElement("div");
+    boardRow.className = "boardRow"
+    board.appendChild(boardRow);
+    for (let x = 0; x<16; x++){
+      let boardSquare = document.createElement("div");
+      boardSquare.className = "boardSquare";
+      console.log("i'm in boardSquare");
+      boardRow.appendChild(boardSquare);
+    }
+  }
+}
+
+initialBoard();
